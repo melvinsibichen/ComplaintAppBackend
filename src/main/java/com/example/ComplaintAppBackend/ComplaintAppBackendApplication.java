24acd1df53ceaa00adfbc225c284542f9df14ce4
melvinsibichen <melvinsibichen1020@gmail.com>
2022-12-09 14:21:27 +0530

    first

---
+++ src/main/java/com/example/ComplaintAppBackend/ComplaintAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ComplaintAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ComplaintAppBackend/ComplaintAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ComplaintAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
